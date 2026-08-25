7d33b2eb8f66522b1896a85760568ad3352f8f7b
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-25 10:55:29 +0530

    registered errormiddlewares in index file, fixed imports, added redis offset for startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import client from './config/redis.config'
-import envConfig from './config/env.config';
+import client from './config/redis.config.js'
+import envConfig from './config/env.config.js';
 
-import mainRouter from './routes/main.routes';
-import redirectRouter from './routes/redirect.route';
+import mainRouter from './routes/main.routes.js';
+import redirectRouter from './routes/redirect.route.js';
+import errorHandler from './middlewares/error.middleware.js';
 
 
 
@@ -16,9 +17,12 @@ app.use('/api', mainRouter)
 app.use('/', redirectRouter)
 
 
+app.use(errorHandler)
+
 const main = async () => {
     try {
         await client.connect()
+        await client.set('idpool:counter', '238328', { NX: true });
         app.listen(PORT, () => {
             console.log(`Server is running on PORT : ${PORT}`)
         })
